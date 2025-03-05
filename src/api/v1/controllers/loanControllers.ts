f8b4bf5e3db4682c0f9a57288eedfd1b62e3e059
Adryan Mackenzie-Hopke <amackenzie-hopke@rrc.ca>
2025-03-05 15:53:14 -0600

    feat:implemented basic controllers to allow sucess responses to be returned app and server and all routes are currently working

---
+++ src/api/v1/controllers/loanControllers.ts
@@ -0,0 +1,54 @@
+import { Request, Response,NextFunction } from "express";
+import { successResponse } from "../models/responseModel";
+
+
+
+
+export const createLoanRequest = async (req: Request, res: Response,next:NextFunction): Promise<void> => {
+    try {
+        console.log("create loan request received");
+        res.status(201).json(
+            successResponse(
+                 "loan request received to system"
+            ));
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateLoanReview = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        console.log("Update loan review received");
+        res.status(200).json(
+            successResponse(
+                "Loan review updated"
+            ));
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getAllLoans = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        console.log("Get all loans request received");
+        res.status(200).json(
+            successResponse(
+                "Loans retrieved"
+            ));
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export const approveUpdateLoan = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        console.log("Approve loan request received");
+        res.status(200).json(
+            successResponse(
+                "Loan approved"
+            ));
+    } catch (error) {
+        next(error);
+    }
+};
